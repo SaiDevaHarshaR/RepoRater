51bfba4b8fe347114e42783bfd4ff6cd68adc5db
BuildTools <unconfigured@null.spigotmc.org>
2023-10-19 08:39:42 +0530

    Initial commit with all files

---
+++ Bookstore/Backend/config.js
@@ -0,0 +1,7 @@
+
+
+
+
+
+export const PORT = '5555';
+export  const mongoDBURL ='mongodb+srv://SaiDevaHarsha:saisurya@cluster0.jwy0dcw.mongodb.net/Cluster0?retryWrites=true&w=majority';
---
+++ Bookstore/Backend/index.js
@@ -0,0 +1,26 @@
+import   express, { request, request, response }  from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
+import { Book }  from "./models/bookModel.js";  // Adjust the path as needed
+import booksRoute from './routes/booksRoute.js';
+
+const app = express(); 
+
+app.use(express.json());
+
+app.get('/', (request, response) =>{
+    console.log(request)
+return response.status(234).send("Running and good going!")
+});
+
+app.use('/books',booksRoute);
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('App connected to Database!');
+    app.listen(PORT, () =>{
+        console.log(`App is listening to port: ${PORT}`);
+
+    });
+}).catch((error)=>{
+    console.log('Error!');
+});
